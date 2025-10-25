827e93a21522aa3663d4a4d7f45ca17df6b4126e
sierra el gihub <l24170701@culiacan.tecnm.mx>
2025-10-25 17:11:19 +0000

    avance

--- miPrincipal/modelo/Libro.java
+++ miPrincipal/modelo/Libro.java
@@ -1,5 +1,43 @@
 package miPrincipal.modelo;
 
 public class Libro {
+    private String titulo;
+    private String autor;
+    private String isbn;
     
+    public Libro(String titulo, String autor, String isbn) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+    }
+    
+    // Getters
+    public String getTitulo() {
+        return titulo;
+    }
+    
+    public String getAutor() {
+        return autor;
+    }
+    
+    public String getIsbn() {
+        return isbn;
+    }
+    
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Libro libro = (Libro) obj;
+        return isbn.equals(libro.isbn);
+    }
 }
--- miPrincipal/modelo/Pedido.java
+++ miPrincipal/modelo/Pedido.java
@@ -1,10 +1,29 @@
 package miPrincipal.modelo;
-
 import utilerias.Fecha;
 
 public class Pedido {
     private Libro libro;
     private Fecha fechaPedido;
 
-   
+    public Pedido(Libro libro, Fecha fechaPedido) {
+        this.libro = libro;
+        this.fechaPedido = fechaPedido;
+    }
+    
+    // Getters
+    public Libro getLibro() {
+        return libro;
+    }
+    
+    public Fecha getFecha() {
+        return fechaPedido;
+    }
+    
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "libro=" + libro +
+                ", fechaPedido=" + fechaPedido +
+                '}';
+    }
 }
